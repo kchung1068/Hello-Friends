a1a31652fad9ec4dca3dabb528c00450433c77e6
Kyle Chung <kchung1068@stu.d214.org>
2019-08-14 14:17:54 -0500

    Initial Commit

---
+++ Hello Friends/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello Friends
+//
+//  Created by Kyle C on 8/14/19.
+//  Copyright © 2019 Kyle C. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
